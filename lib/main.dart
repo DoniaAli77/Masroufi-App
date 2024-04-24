4d6781675675490b391315f182b8d4a25e23b52e
DoniaAli77 <67129196+DoniaAli77@users.noreply.github.com>
2024-04-24 13:46:14 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Masroufi',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,

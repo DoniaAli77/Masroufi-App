a52a50e0fdafda64a274c1aba63c09504a58876f
DoniaAli77 <67129196+DoniaAli77@users.noreply.github.com>
2024-06-23 14:55:42 +0300

    Update NewExWidget.dart

--- lib/NewExWidget.dart
+++ lib/NewExWidget.dart
@@ -74,72 +74,72 @@ class _ExpenseFormState extends State<ExpenseForm> {
     );
   }
 }
-//------------------(hook version still under testing how to pass context )-----------------
+//------------------(hook version)-----------------
 
-// class NewExWidgetHook extends HookWidget {
-//   Function addnew;
-//   NewExWidgetHook({required this.addnew});
-//   @override
-//   Widget build(BuildContext context) {
+class ExpenseFormHook extends HookWidget {
+  Function addnew;
+  ExpenseFormHook({required this.addnew});
+  @override
+  Widget build(BuildContext context) {
+    var title = useTextEditingController();
+    var amount = useTextEditingController();
+    ValueNotifier<DateTime> seDate = useState(DateTime.utc(1970, 1, 1));
+    void opencalender() {
+      showDatePicker(
+              context: context,
+              initialDate: DateTime.now(),
+              firstDate: DateTime.now(),
+              lastDate: DateTime.utc(2024, 12, 30))
+          .then((pickedDate) {
+        if (pickedDate == null) return;
 
-//     var title = useTextEditingController();
-//     var amount = useTextEditingController();
-//     ValueNotifier<DateTime> seDate = useState(DateTime.utc(1970, 1, 1));
-//     // DateTime seDate = DateTime.utc(1970, 1, 1);
-//     void opencalender() {
-//       showDatePicker(
-//               context: context,
-//               initialDate: DateTime.now(),
-//               firstDate: DateTime.now(),
-//               lastDate: DateTime.utc(2024, 12, 30))
-//           .then((pickedDate) {
-//         if (pickedDate == null) return;
+        seDate.value = pickedDate;
+      });
+    }
 
-//         seDate.value = pickedDate;
-//       });
-//     }
-
-//     return SingleChildScrollView(
-//       child: Container(
-//         margin: EdgeInsets.only(
-//             left: 10,
-//             right: 10,
-//             bottom: MediaQuery.of(context).viewInsets.bottom + 10),
-//         child: Column(
-//           crossAxisAlignment: CrossAxisAlignment.end,
-//           children: [
-//             TextField(
-//               decoration: InputDecoration(labelText: 'ex title'),
-//               controller: title,
-//             ),
-//             TextField(
-//               decoration: InputDecoration(labelText: 'amount'),
-//               controller: amount,
-//             ),
-//             Row(
-//               children: [
-//                 Expanded(
-//                     child: Text(seDate.value == DateTime.utc(1970, 1, 1)
-//                         ? 'no choosen Date'
-//                         : DateFormat.yMd().format(seDate.value))),
-//                 TextButton(
-//                     onPressed: () {
-//                       opencalender();
-//                     },
-//                     child: Text('choose date'))
-//               ],
-//             ),
-//             ElevatedButton(
-//                 onPressed: () {
-//                   addnew(
-//                       t: title.value, a: double.parse(amount.value as String), d: seDate,mycontext:context);
-//                   title.clear();
-//                   amount.clear();
-//                 },
-//                 child: Text('submit'))
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
+    return SingleChildScrollView(
+      child: Container(
+        margin: EdgeInsets.only(
+            left: 10,
+            right: 10,
+            bottom: MediaQuery.of(context).viewInsets.bottom + 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            TextField(
+              decoration: InputDecoration(labelText: 'ex title'),
+              controller: title,
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'amount'),
+              controller: amount,
+            ),
+            Row(
+              children: [
+                Expanded(
+                    child: Text(seDate.value == DateTime.utc(1970, 1, 1)
+                        ? 'no choosen Date'
+                        : DateFormat.yMd().format(seDate.value))),
+                TextButton(
+                    onPressed: () {
+                      opencalender();
+                    },
+                    child: Text('choose date'))
+              ],
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  this.addnew(
+                      t: title.text,
+                      a: double.parse(amount.text),
+                      d: seDate.value);
+                  title.clear();
+                  amount.clear();
+                },
+                child: Text('submit'))
+          ],
+        ),
+      ),
+    );
+  }
+}
